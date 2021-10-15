ee80da0fccb4694c9ab5bb7e437b27a9b71733a7
Alisson98 <alissonfernandes98@outlook.com>
2021-10-15 16:37:54 -0300

    dev profile implementation

---
+++ src/main/java/fernandes/alisson/sosystem/config/DevProfileConfig.java
@@ -0,0 +1,25 @@
+package fernandes.alisson.sosystem.config;
+
+import fernandes.alisson.sosystem.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevProfileConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddl;
+    @Bean
+    public boolean instancyDB (){
+        if(ddl.equals("create")) this.dbService.instancyDB();
+        return false;
+    }
+
+
+}
