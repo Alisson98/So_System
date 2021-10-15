3c169eeac42d5c115d2ceac9534136c02e4e4f00
Alisson98 <alissonfernandes98@outlook.com>
2021-10-15 15:34:16 -0300

    instantiated objects t1/c1/os1

--- src/main/java/fernandes/alisson/sosystem/SosystemApplication.java
+++ src/main/java/fernandes/alisson/sosystem/SosystemApplication.java
@@ -1,13 +1,27 @@
 package fernandes.alisson.sosystem;
 
+import fernandes.alisson.sosystem.domain.Client;
+import fernandes.alisson.sosystem.domain.Os;
+import fernandes.alisson.sosystem.domain.Technician;
+import fernandes.alisson.sosystem.domain.enuns.Priority;
+import fernandes.alisson.sosystem.domain.enuns.Status;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SosystemApplication {
+public class SosystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SosystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Technician t1 = new Technician(null, "Alisson","654.432.120-60", "(99) 99999999");
+		Client c1 = new Client(null, "Betina Campos", "881.259.350-02", "(99) 99999999" );
+		Os os1= new Os(null, Priority.HIGH, Status.PROGRESS, "Teste create OS", t1, c1);
+
+
+	}
 }
