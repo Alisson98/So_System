6f16e6a7162a968596e9847b1e28fcac120d9c30
Alisson98 <alissonfernandes98@outlook.com>
2021-10-15 11:35:06 -0300

    first commit

---
+++ src/main/java/fernandes/alisson/sosystem/SosystemApplication.java
@@ -0,0 +1,13 @@
+package fernandes.alisson.sosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fernandes/alisson/sosystem/SosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fernandes.alisson.sosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
