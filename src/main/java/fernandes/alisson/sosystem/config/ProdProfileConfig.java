00fca2f0e130d8f7314e766f3f84fc6105df8a6c
Alisson98 <alissonfernandes98@outlook.com>
2021-11-16 15:47:31 -0300

    prod profile and mysql driver implemented.

---
+++ src/main/java/fernandes/alisson/sosystem/config/ProdProfileConfig.java
@@ -0,0 +1,25 @@
+package fernandes.alisson.sosystem.config;
+
+import fernandes.alisson.sosystem.domain.usecases.DBService.impl.DBServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class ProdProfileConfig {
+    @Autowired
+    private DBServiceImpl dbServiceImpl;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddl;
+    @Bean
+    public boolean instancyDB (){
+        if(ddl.equals("create-drop")) this.dbServiceImpl.instancyDB();
+        return false;
+    }
+
+
+}
