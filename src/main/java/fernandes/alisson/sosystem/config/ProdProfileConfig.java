e5fe4a9f8a9d538d180c48c0d6ab1e2b43e33f53
Alisson98 <alissonfernandes98@outlook.com>
2021-11-18 14:19:12 -0300

    fixed pointing in profile

--- src/main/java/fernandes/alisson/sosystem/config/ProdProfileConfig.java
+++ src/main/java/fernandes/alisson/sosystem/config/ProdProfileConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("prod")
 public class ProdProfileConfig {
     @Autowired
     private DBServiceImpl dbServiceImpl;
